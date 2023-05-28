fe95b7a635faa8ece6228762f6e87b1b6267da5e
Oleh Protsenko <designofhumanity@gmail.com>
2023-05-28 12:40:11 +0300

    fix option reset

--- src/customComponents/OptionsTest.js
+++ src/customComponents/OptionsTest.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 
 function OptionsTest({ options, onSelect }) {
-    return (
-        <Form.Group>
-            {Object.entries(options).map(([label, value]) => (
-                <Form.Check
-                    key={label}
-                    type="radio"
-                    label={label}
-                    onChange={() => onSelect(value)}
-                />
-            ))}
-        </Form.Group>
-    );
+ const [selectedOption, setSelectedOption] = React.useState(null);
+
+ React.useEffect(() => {
+ setSelectedOption(null);
+ }, [options]);
+
+ return (
+ <Form.Group>
+ {Object.entries(options).map(([label, value]) => (
+ <Form.Check
+ key={label}
+ type="radio"
+ label={label}
+ checked={selectedOption === value}
+ onChange={() => {
+ setSelectedOption(value);
+ onSelect(value);
+ }}
+ />
+ ))}
+ </Form.Group>
+ );
 }
 
 export default OptionsTest;
