4d4c71da599494cc61317239474773ee6f5cf3f1
Oleh Protsenko <designofhumanity@gmail.com>
2023-06-03 16:31:22 +0300

    fix

--- src/customComponents/ProgressBar.js
+++ src/customComponents/ProgressBar.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { ProgressBar } from 'react-bootstrap';
 
 function TestProgressBar({ currentStep, totalSteps }) {
- const progress = (currentStep / totalSteps) * 100;
+    const progress = (currentStep / totalSteps) * 100;
 
- return (
- <>
- <p>
- Вопрос №{currentStep + 1} из {totalSteps}
- </p>
- <ProgressBar now={progress} />
- </>
- );
+    return (
+        <>
+            <p>
+                Вопрос №{currentStep + 1} из {totalSteps}
+            </p>
+            <ProgressBar now={progress} />
+        </>
+    );
 }
 
 export default TestProgressBar;
