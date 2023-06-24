d87152f9984110654c3af9d48fdced1095a3618b
Oleh Protsenko <designofhumanity@gmail.com>
2023-06-24 19:20:56 +0300

    translation of questions

---
+++ public/translate_questions.py
@@ -0,0 +1,54 @@
+import json
+from googletrans import Translator
+
+# Define the 10 most spoken languages in the world
+popular_languages = {
+    'english': 'en',
+    'mandarin': 'zh-cn',
+    'hindi': 'hi',
+    'spanish': 'es',
+    'french': 'fr',
+    'arabic': 'ar',
+    'bengali': 'bn',
+    'russian': 'ru',
+    'portuguese': 'pt',
+    'indonesian': 'id'
+}
+
+translator = Translator(service_urls=['translate.google.com'])
+
+# Open the questions.json file and load the content
+with open('questions.json', 'r') as f:
+    data = json.load(f)
+
+for lang in popular_languages.values():
+    # Create a new json file for each language
+    with open(f'questions_{lang}.json', 'w') as f:
+        translated_data = []
+        for question in data:
+            translated_question = question.copy()  # Make a copy of the original question
+
+            print(f"Translating question: {question['label']} to language: {lang}")
+
+            # Translate the label of the question
+            try:
+                translated_text = translator.translate(question['label'], dest=lang).text
+            except Exception as e:
+                translated_text = question['label']
+                print(f"Translation error occurred: {str(e)}")
+            translated_question['label'] = translated_text
+
+            # Translate the options
+            translated_options = {}
+            for option, option_data in question['options'].items():
+                try:
+                    translated_text = translator.translate(option, dest=lang).text
+                except Exception as e:
+                    translated_text = option
+                    print(f"Translation error occurred: {str(e)}")
+                translated_options[translated_text] = option_data
+
+            translated_question['options'] = translated_options
+
+            translated_data.append(translated_question)
+        json.dump(translated_data, f, ensure_ascii=False, indent=4)
