8923a4b44b29f7aa6f39f5652365c25ead854a7b
Oleh Protsenko <designofhumanity@gmail.com>
2023-06-24 19:27:27 +0300

    caching mechanism

--- public/translate_questions.py
+++ public/translate_questions.py
@@ -46,6 +46,9 @@ translator = Translator(service_urls=['translate.google.com'], timeout=5)  # Set
 with open('questions.json', 'r') as f:
     data = json.load(f)
 
+# Cache dictionary to store translated texts
+translation_cache = {}
+
 for lang in popular_languages.values():
     # Create a new json file for each language
     with open(f'questions_{lang}.json', 'w') as f:
@@ -55,26 +58,36 @@ for lang in popular_languages.values():
 
             print(f"Translating question: {question['label']} to language: {lang}")
 
-            # Translate the label of the question
-            try:
-                translated_text = translator.translate(question['label'], dest=lang).text
-            except Exception as e:
-                translated_text = question['label']
-                print(f"Translation error occurred: {str(e)}")
+            # Check if the translation is already cached
+            if (question['label'], lang) in translation_cache:
+                translated_text = translation_cache[(question['label'], lang)]
+            else:
+                # Translate the label of the question
+                try:
+                    translated_text = translator.translate(question['label'], dest=lang).text
+                    translation_cache[(question['label'], lang)] = translated_text  # Cache the translation
+                except Exception as e:
+                    translated_text = question['label']
+                    print(f"Translation error occurred: {str(e)}")
+
             translated_question['label'] = translated_text
 
             # Translate the options
             translated_options = {}
             for option, option_data in question['options'].items():
-                try:
-                    translated_text = translator.translate(option, dest=lang).text
-                except Exception as e:
-                    translated_text = option
-                    print(f"Translation error occurred: {str(e)}")
-                translated_options[translated_text] = option_data
+                if (option, lang) in translation_cache:
+                    translated_option = translation_cache[(option, lang)]
+                else:
+                    try:
+                        translated_option = translator.translate(option, dest=lang).text
+                        translation_cache[(option, lang)] = translated_option
+                    except Exception as e:
+                        translated_option = option
+                        print(f"Translation error occurred: {str(e)}")
+
+                translated_options[translated_option] = option_data
 
             translated_question['options'] = translated_options
 
             translated_data.append(translated_question)
-            time.sleep(1)  # Add a delay between each translation to avoid overwhelming the translation service
-        json.dump(translated_data, f, ensure_ascii=False, indent=4)
+            time.sleep(1)  # Add a delay between
