a698ff3caca8a5e885092cd32b4d9ee35c87cfe6
Oleh Protsenko <designofhumanity@gmail.com>
2023-06-25 12:24:51 +0300

    safas

--- src/customComponents/PeopleNearby.js
+++ src/customComponents/PeopleNearby.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import mockPeople from './mockData/mockPeople.json';
+import mockPeople from './mock.json';
 
 const PeopleNearby = () => {
   const [loading, setLoading] = useState(true);
